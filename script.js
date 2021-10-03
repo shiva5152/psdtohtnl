23bfe3eed0ee183722e2c51a9a8f4db0d23b4f03
Shiva <ms4544311@gmail.com>
2021-10-03 15:30:29 +0530

    psdtohtml

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu =  document.getElementById("mob-menu");
+
+
+
+function openMenu(){
+   menu.style.left = "0px"
+
+}
+
+function closeMenu(){
+    menu.style.left = "-250px"
+   
+}
+
+
+
+   
